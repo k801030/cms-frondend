30d581d6f19b5a043c1bfa16b87148017a71c67b
Vison <kk801030@gmail.com>
2015-04-17 12:24:44 +0800

    side-bar added

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '臺大軍訓室' });
 });
 
 router.get('/post/:id', function(req, res, next) {
   var id = req.params.id;
-  res.render('single-post', { title: 'Express' , post_id: id});
+  res.render('single-post', { title: '臺大軍訓室' , post_id: id});
 });
 
 
